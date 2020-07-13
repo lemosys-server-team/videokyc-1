1a5273541b9d422407756cea506c67b9c31e64d6
developer <developer>
2020-07-13 20:09:04 +0530

    worked on frontend

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Cache;
 
 use App\User;
 use App\Setting;
+use App\Holiday;
+use App\Time;
+use App\Schedule;
 
 /**
 * Method Name : setSetting 
@@ -42,4 +45,27 @@ function getSetting($option_name){
     }
     return '';
 }
+
+function saleAvailability($date=null,$sale_id=null,$time=null){
+    $holidays=Holiday::where('date', '=',$date)->first();
+    if(isset($holidays)){
+        return 'false ';
+    }else{
+        $user=User::where('id',$sale_id)->first();
+        $time_id=isset($user->time_id)?$user->time_id:0;
+        $breack_time = date("H:i:s",strtotime('+1 minutes', strtotime($time)));
+        $times=Time::where('id',$time_id)->whereRaw("('$time' BETWEEN start_time AND  end_time)")->whereRaw("('$breack_time' NOT BETWEEN break_start_time AND  break_end_time)")->first();
+        if(isset($times)){
+            $records=Schedule::where('datetime', '=',$date)->where('sale_id',$sale_id)->first();
+            if(isset($records)){
+                return 'false';
+            }else{
+                return 'true';
+            }
+        }else{
+            return 'false ';
+        }
+    }
+}
+
 ?>
