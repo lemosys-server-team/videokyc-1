88adbaf78df42904179a92ee32a5669f925a46e9
developer <developer>
2020-07-14 12:47:50 +0530

    worked on fronetend

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -47,14 +47,15 @@ function getSetting($option_name){
 }
 
 function saleAvailability($date=null,$sale_id=null,$time=null){
-    $holidays=Holiday::where('date', '=',$date)->first();
+    $holidays=Holiday::where('date', '=',$date)->where('is_active',true)->first();
     if(isset($holidays)){
         return 'false ';
     }else{
         $user=User::where('id',$sale_id)->first();
         $time_id=isset($user->time_id)?$user->time_id:0;
         $breack_time = date("H:i:s",strtotime('+1 minutes', strtotime($time)));
-        $times=Time::where('id',$time_id)->whereRaw("('$time' BETWEEN start_time AND  end_time)")->whereRaw("('$breack_time' NOT BETWEEN break_start_time AND  break_end_time)")->first();
+        //$times=Time::where('id',$time_id)->where('is_active',true)->whereRaw("('$time' BETWEEN start_time AND  end_time)")->whereRaw("('$breack_time' NOT BETWEEN break_start_time AND  break_end_time)")->first();
+        $times=Time::where('id',$time_id)->whereRaw("('$time' BETWEEN start_time AND  end_time)")->whereRaw("CASE WHEN IFNULL(break_start_time,'')!='' THEN ('$breack_time' NOT BETWEEN break_start_time AND break_end_time) ELSE 1 END")->first();
         if(isset($times)){
             $records=Schedule::where('datetime', '=',$date)->where('sale_id',$sale_id)->first();
             if(isset($records)){
