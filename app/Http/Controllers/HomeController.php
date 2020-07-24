61d8cf92c0021c8c872e0f5a7021e41ff8e3820d
charul1234 <chandrakala.patidar@lemosys.com>
2020-07-24 15:00:03 +0530

    upload home chnages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -136,7 +136,7 @@ class HomeController extends Controller
         $startTime =Time::where('is_active',TRUE)->min('start_time');
       	$start_time=Time::where('is_active',TRUE)->min('start_time');
         $end_time=Time::where('is_active',TRUE)->max('end_time');
-        if (isset($request->date) && strtotime(date('Y-m-d', strtotime($request->date))==strtotime(date('Y-m-d'))) {
+        if (isset($request->date) && strtotime(date('Y-m-d', strtotime($request->date))==strtotime(date('Y-m-d')))) {
             $startTime=date('H:i:s');
         }
         $date=isset($request->date)?$request->date:date('Y-m-d');
